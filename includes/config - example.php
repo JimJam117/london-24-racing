c496a464d28d1865bf9cab54494ad44efc92bce5
JimJam117 <jamessparrow101@gmail.com>
2020-01-29 07:03:25 +0000

    Added example config file

---
+++ includes/config - example.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James
+ * Date: 05/08/2017
+ * Time: 17:08
+ **/
+
+define("DB_HOST", "localhost");
+define("DB_USER", "database-user-here");
+define("DB_PASS", 'database-password-here');
+define("DB_NAME", "database-name-here");
+
+$title = "";
+$description = "";
+
+
+?>
